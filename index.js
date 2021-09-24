7925685bc9f79ab74b95bd8fc8769ac32683399d
MaksAbrikosov <maksim.abrikosov@delvedeeper.com>
2021-09-24 11:06:01 +0200

    browser tab opened automatically after running

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser')
 const exphbs = require('express-handlebars')
 const creativeRoutes = require('./routes/creative')
 const progressUpload = require("./functions/progressUpload")
+const open = require('open');
 
 const PORT = 5000
 const app = express()
@@ -39,4 +40,7 @@ app.use('/css', express.static(__dirname + '/views/assets/css'));
 
 app.listen(PORT, ()=> {
     console.log(`Server has been started on port ${PORT}...`)
+    // open(`http://localhost:${PORT}/`, {app: {name: 'google chrome', }})
+    open(`http://localhost:${PORT}/`)
+
 })
