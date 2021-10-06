5f7f611a9205e6d658c2ef303fc7bf8e5e3a5dae
MaksAbrikosov <maksim.abrikosov@delvedeeper.com>
2021-10-06 12:56:09 +0200

    fixed get campaign id

--- functions/getCampaign.js
+++ functions/getCampaign.js
@@ -20,10 +20,33 @@ function getCampaign(accountParameters, xsrfToken){
         }
     ]
 
+    const argumentsIfCampaignEmpty = [
+        {
+            "id":accountParameters.advertiserId,
+            "entityKey":{
+                "entityId":accountParameters.advertiserId,
+                "ownerId":accountParameters.ownerId
+            }
+        },
+        {
+            "id":accountParameters.advertiserId,
+            "entityKey":{
+                "entityId":accountParameters.advertiserId,
+                "ownerId":accountParameters.ownerId
+            }
+        }
+    ]
+
     const payload = {
         service: 'CampaignService',
         method: 'get',
         arguments: arguments.map(JSON.stringify)
+    }
+
+    const payloadIfCampaignEmpty = {
+        service: 'CampaignService',
+        method: 'get',
+        arguments: argumentsIfCampaignEmpty.map(JSON.stringify)
     }
 
     return axios({
@@ -33,12 +56,6 @@ function getCampaign(accountParameters, xsrfToken){
             "accept": "application/json, text/plain, */*",
             "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
             "content-type": "text/plain",
-            // "sec-ch-ua": "\" Not;A Brand\";v=\"99\", \"Google Chrome\";v=\"91\", \"Chromium\";v=\"91\"",
-            // "sec-ch-ua-mobile": "?0",
-            // "sec-fetch-dest": "empty",
-            // "sec-fetch-mode": "cors",
-            // "sec-fetch-site": "same-origin",
-            // "x-client-data": "CI22yQEIpLbJAQjEtskBCKmdygEIuv3KAQigoMsBCNzyywE=",
             "x-xsrf-token": xsrfToken,
             "cookie": `${config.Secure3PSID} ${config.SID}`,
         },
@@ -50,7 +67,33 @@ function getCampaign(accountParameters, xsrfToken){
         .then(response => {
             return response.data
         })
-        .catch(err => console.error(err.response));
+        // .catch(err => console.error(err.response));
+        .catch(() => {
+
+            ifCampaignEmpty(accountParameters, xsrfToken, payloadIfCampaignEmpty)
+                .then(response => {
+                    return response.data
+                 })
+                .catch(err => console.error(err.response));
+        });
+}
+
+function ifCampaignEmpty(accountParameters, xsrfToken, payloadIfCampaignEmpty){
+    return axios({
+        method: 'post',
+        url: "https://www.google.com/doubleclick/studio/service",
+        headers: {
+            "accept": "application/json, text/plain, */*",
+            "accept-language": "en-GB,en-US;q=0.9,en;q=0.8",
+            "content-type": "text/plain",
+            "x-xsrf-token": xsrfToken,
+            "cookie": `${config.Secure3PSID} ${config.SID}`,
+        },
+        referrer: "https://www.google.com/doubleclick/studio/",
+        referrerPolicy: "strict-origin-when-cross-origin",
+        data: JSON.stringify(payloadIfCampaignEmpty),
+        mode: "cors",
+    })
 }
 
 module.exports = getCampaign;
--- views/assets/js/parseCampaignUrl.js
+++ views/assets/js/parseCampaignUrl.js
@@ -9,10 +9,14 @@ function parseCampaignUrl() {
     }
 
     // const campaignId = creativeUrlValue.slice(creativeUrlValue.indexOf('campaignId='), creativeUrlValue.indexOf('&')).split("=")[1]
-    const parseUrl = creativeUrlValue.split(/\=|&/)
-    const campaignId = parseUrl[1]
-    const advertiserId = parseUrl[3]
-    const ownerId = parseUrl[5]
+    // const parseUrl = creativeUrlValue.split(/\=|&/)
+    // const campaignId = parseUrl[1]
+    // const advertiserId = parseUrl[3]
+    // const ownerId = parseUrl[5]
+
+    const advertiserId = creativeUrlValue.match(/advertiserId=([^&]+)/)[1]
+    const campaignId = creativeUrlValue.match(/campaignId=([^&]+)/)[1]
+    const ownerId = creativeUrlValue.match(/ownerId=([^&]+)/)[1]
 
     return {
         campaignId,
